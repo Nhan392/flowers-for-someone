60aa7f5cdf78a771ef670932c50a6c004a6d84e8
Nhan392 <112955603+Nhan392@users.noreply.github.com>
2024-07-23 23:33:55 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HAPPY BIRTHDAY').split('')
+    const titles = ('HAPPY BIRTHDAY TO U').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
